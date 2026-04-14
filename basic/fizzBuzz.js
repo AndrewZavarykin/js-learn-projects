9d330aeab3319a472d88be206bea035a3e3d7684
andrew zavarykin <andrewzavarykin@gmail.com>
2026-04-14 12:54:15 -0700

    fix fizzBuzz

--- basic/fizzBuzz.js
+++ basic/fizzBuzz.js
@@ -11,7 +11,7 @@
  * https://gist.github.com/vvscode/5b5eec5bb5270de3ac27a3e0298430d9
  */
 function fizzBuzz() {
-  for (let i = 1; i<101; i++) {
+  for (let i = 1; i <= 100; i++) {
     if (i % 15 === 0) console.log('FizzBuzz');
     else if (i % 5 === 0) console.log('Buzz');
     else if (i % 3 === 0) console.log('Fizz');
