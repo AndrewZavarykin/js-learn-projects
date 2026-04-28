c395e97c09321cdb700605fc552fbe76940299dc
andrew zavarykin <andrewzavarykin@gmail.com>
2026-04-28 11:56:44 -0700

    add tests

---
+++ test-project/paragraphs.js
@@ -0,0 +1,101 @@
+/**
+ * Написать скрипт для реализации следующего приложения.
+ * На странице должны быть три текстовых параграфа, поле ввода и кнопка.
+ * Напишите скрипт, который будет выполнять следующие условия: 
+ *  1.Кнопка скрыта, если в поле ввода нет значения. 
+ *  2.При клике на кнопку добавляется новый параграф, содержащий текст из поля ввода. 
+ *  3.Если параграфов становится больше 4, первый из них удаляется.
+ */
+
+function renderParagraphs() {
+    const container = document.createElement("div");
+    container.className = "container";
+    for (let i=0; i<3; i++) {
+        const paragraph = document.createElement("p");
+        paragraph.textContent = i;
+        paragraph.className = "par-item";
+        container.appendChild(paragraph);
+    }
+    document.body.appendChild(container);
+}
+
+function renderInput() {
+    const inputEl = document.createElement("input");
+    inputEl.id = "input";
+    inputEl.className = "input";
+    inputEl.type = "text";
+    document.body.appendChild(inputEl);
+}
+
+function renderButton() {
+    const buttonEl = document.createElement("button");
+    buttonEl.className = "button";
+    buttonEl.textContent = "Add paragraph";
+    buttonEl.hidden = true;
+    document.body.appendChild(buttonEl);
+}
+
+function setEventListenerOnInput() {
+    const input = document.querySelector("#input");
+
+    // Инициализация состояния кнопки при загрузке
+    checkButtonVisibility();
+    input.addEventListener("input", checkButtonVisibility);
+}
+
+function checkButtonVisibility() {
+    const value = document.querySelector("#input").value;
+    const button = document.querySelector('.button');
+    if (value.trim().length > 0) {
+        button.hidden = false;
+    } else {
+        button.hidden = true;
+    }
+}
+
+function setEventListenerOnButton() {
+    const button = document.querySelector(".button");
+    button.addEventListener("click", buttonHandler);
+}
+
+function buttonHandler(event) {
+    const text = document.getElementById("input").value;
+    if (text.trim().length > 0) {
+        addParagraph(text);
+        clearInput();
+        removeFirstParagraph();
+    }
+}
+
+function addParagraph(text) {
+    const paragraph = document.createElement("p");
+    paragraph.textContent = text;
+    paragraph.className = "par-item";
+    document.querySelector(".container").appendChild(paragraph);
+}
+
+function clearInput() {
+    const inputEl = document.querySelector(".input");
+    const buttonEl = document.querySelector(".button");
+    inputEl.value = "";
+    buttonEl.hidden = true;
+}
+
+function removeFirstParagraph() {
+    const paragraphsConteiner = document.querySelector(".container");
+    if (paragraphsConteiner.childNodes.length > 4) {
+        const firstParagraph = paragraphsConteiner.firstChild;
+        firstParagraph.remove();
+    }
+}
+
+export function initApp() {
+    renderParagraphs();
+    renderInput();
+    renderButton();
+    setEventListenerOnInput();
+    setEventListenerOnButton();
+};
+
+initApp();
+
---
+++ test-project/paragraphs.test.js
@@ -0,0 +1,52 @@
+import { initApp } from "./paragraphs";
+
+describe("paragraphs test", () => {
+    
+    initApp();
+
+    test("scriptShouldRenderDivContainerWithClassContainer", () => {
+        expect(document.querySelector(".container")).notBeNull;
+    })
+
+    test("divContainerShouldHaveThreeParagraphsWithClassParItem", () => {
+        const container = document.querySelector(".container");
+        const children = container.children;
+        expect(children).toHaveLength(3);
+        for (let child of children) {
+            expect(child.tagName).toMatch("P");
+            expect(child.className).toMatch("par-item");
+        }
+    })
+
+    test("scriptShouldRenderInputWithIdAndClassInputAndTypeText", () => {
+        const input = document.querySelector(".input");
+        expect(input).notBeNull;
+        expect(input.type).toBe("text");
+        expect(input.id).toBe("input");
+        expect(input.className).toBe("input");
+    })
+
+    test("scriptShouldRenderButtonWithClassButtonAndTextAddParagraphAndHiddenTrue", () => {
+        const button = document.querySelector(".button");
+        expect(button).notBeNull;
+        expect(button.textContent).toBe("Add paragraph");
+        expect(button.hidden).toBeTruthy();
+
+    })
+
+    test("buttonShouldBeHiddenTrueWhenInputEmpty", () => {
+        const input = document.querySelector("#input");
+        input.value = "  ";
+        const button = document.querySelector(".button");
+        input.dispatchEvent(new Event('input'));
+        expect(button.hidden).toBe(true);
+    })
+
+    test("buttonShouldBeHiddenFalseWhenInputNotEmpty", () => {
+        const input = document.querySelector("#input");
+        input.value = "123";
+        const button = document.querySelector(".button");
+        input.dispatchEvent(new Event('input'));
+        expect(button.hidden).toBe(false);
+    })
+});
