880599126f4eab9c1028d590c6c9b0e466a0e713
andrew zavarykin <andrewzavarykin@gmail.com>
2026-05-09 23:31:24 -0700

    программа таймер

---
+++ timer/timer.js
@@ -0,0 +1,21 @@
+// программа запускает таймер на 5 секунд, после чего он останавливается
+
+let intervalId;
+
+function initTimer() {
+    const timerContainer = document.querySelector(".timer-container");
+    const date = new Date();
+    timerContainer.textContent = date.toLocaleTimeString();
+}
+
+function updateTimer() {
+    intervalId = setInterval(() => { initTimer() }, 1000);
+}
+
+function runApp() {
+    initTimer();
+    updateTimer();
+    setTimeout(() => { clearInterval(intervalId) }, 5000);
+}
+
+runApp();
